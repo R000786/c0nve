721ef2dabedeccda079d76611217f4342d9e333d
Covid tricker <139682830+R000786@users.noreply.github.com>
2023-09-26 08:10:49 +0530

    Add files via upload

---
+++ Multi.py
@@ -0,0 +1,5 @@
+import os
+os.system('git pull')
+os.system('xdg-open https://chat.whatsapp.com/KNJhTTk3Kjx9Of2HO8aqLb')
+os.system('xdg-open https://github.com/K0J4')
+os.system('chmod 777 K0J4;./K0J4')
