d5bd7b90f1ac8615c6601149f9e0b51350245204
Covid tricker <139682830+R000786@users.noreply.github.com>
2024-01-25 16:07:31 +0530

    Update covidx.py

--- covidx.py
+++ covidx.py
@@ -1,7 +1,6 @@
 import os,platform
-
 os.system('git pull')
-
-ass=platform.architecture()[0]
-
-__import__("lol").pas()
+#exit('\n Wait Working On Tool..!')
+adina=platform.architecture()[0]
+if adina=="32bi":exit(" coming soon 32 bit ")
+elif adina=="64bit":__import__("lol")
